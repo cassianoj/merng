e6f7570bd9de576ac0255295b0a9ec88cc4aa25d
João Sidney <you@example.comjs.argolo@gmail.com>
2023-03-14 18:44:20 -0300

    Delete post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,6 @@
-const { Mutation, Query} = require('../typeDefs');
+const { AuthenticationError } = require('apollo-server');
+
+// const { Mutation, Query} = require('../typeDefs');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 
@@ -6,7 +8,7 @@ module.exports = {
     Query: {
         async getPosts(){
             try{
-                const posts = await Post.find();
+                const posts = await Post.find().sort({createdAt: 'desc'});
                 return posts;
             } catch(err){
                 throw new Error(err);
@@ -29,7 +31,6 @@ module.exports = {
     Mutation: {
         async createPost(_, { body}, context){
             const user = checkAuth(context)
-            console.log(user)
 
             const newPost = new Post({
                 body,
@@ -41,6 +42,22 @@ module.exports = {
             const post = await newPost.save();
 
             return post;
+        },
+        async deletePost(_, {postId}, context){
+            const user = checkAuth(context)
+            
+            try{
+                const post = await Post.findById(postId)
+                if (user.username === post.username){
+                    await post.deleteOne();
+                    return 'Post deleted sucessfully'
+                }
+                else {
+                    throw new AuthenticationError('Action not allowed')
+                }
+            } catch (err) {
+                throw new Error(err)
+            }
         }
     }
 }
